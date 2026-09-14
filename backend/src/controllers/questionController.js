d4bfdcbcd89d362837f5e9ccdd76c109b87acb3f
Natnial Asegdew <natnial99@gmail.com>
2026-09-14 14:25:17 +0300

    import ai service and enviroment variable

--- backend/src/controllers/questionController.js
+++ backend/src/controllers/questionController.js
@@ -7,3 +7,8 @@ import {
     deleteQuestionService,
     updateQuestionService,
 } from "../services/questionService.js";
+import {
+    generateQuestionDraftCoachService,
+    assessAnswerAgainstQuestionService,
+} from "../services/aiService.js";
+import env from "../config/env.js";
