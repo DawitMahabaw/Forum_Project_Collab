c63ddd3cd5eb9418b133d8835794fd7037b6e5c4
DawitMahabaw <dawitmahabaw03@gmail.com>
2026-09-14 12:57:32 -0700

    Connect Sidebar to shared Layout

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,17 +1,22 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../Navbar/Navbar.jsx";
+import Sidebar from "../Sidebar/Sidebar.jsx";
 import styles from "./Layout.module.css";
 
 // Shared layout for authenticated application pages.
 const Layout = () => {
   return (
     <div className={styles.shell}>
-      <Navbar />
+      <Sidebar />
 
-      <main className={styles.page}>
-        <Outlet />
-      </main>
+      <div className={styles.content}>
+        <Navbar />
+
+        <main className={styles.page}>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
