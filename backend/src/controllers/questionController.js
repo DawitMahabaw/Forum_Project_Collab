ec1fd532c625bc4c946ca897529ba8b4cc707480
Natnial Asegdew <natnial99@gmail.com>
2026-09-14 14:23:50 +0300

    import services

---
+++ backend/src/controllers/questionController.js
@@ -0,0 +1,9 @@
+import {
+    createQuestionWithVectorService,
+    getQuestionsService,
+    getSingleQuestionService,
+    searchQuestionsSemanticService,
+    getSimilarQuestionsService,
+    deleteQuestionService,
+    updateQuestionService,
+} from "../services/questionService.js";
