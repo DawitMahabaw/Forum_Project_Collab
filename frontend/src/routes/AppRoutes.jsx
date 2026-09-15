200228342d100c359feee4ae1896e71bb660daf7
DawitMahabaw <dawitmahabaw03@gmail.com>
2026-09-14 18:07:18 -0700

    Connect protected pages to shared application layout

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Layout from "../components/Layout/Layout.jsx";
 import ProtectedRoute from "../components/ProtectedRoute.jsx";
+import Layout from "../components/Layout/Layout.jsx";
+
 import AuthPage from "../pages/Auth/AuthPage.jsx";
 import Dashboard from "../pages/Dashboard/Dashboard.jsx";
 import LandingPage from "../pages/Landing/LandingPage.jsx";
@@ -14,7 +15,7 @@ const AppRoutes = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<AuthPage />} />
 
-        {/* Protected application */}
+        {/* Protected application pages */}
         <Route element={<ProtectedRoute />}>
           <Route element={<Layout />}>
             <Route path="/dashboard" element={<Dashboard />} />
