c94ddc92a877456b928a21b773487c2a7162989c
DawitMahabaw <dawitmahabaw03@gmail.com>
2026-09-14 17:56:49 -0700

    Render footer in shared application layout

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 
+import AppFooter from "../AppFooter/AppFooter.jsx";
 import Navbar from "../Navbar/Navbar.jsx";
 import Sidebar from "../Sidebar/Sidebar.jsx";
 import styles from "./Layout.module.css";
@@ -15,6 +16,9 @@ const Layout = () => {
 
         <main className={styles.page}>
           <Outlet />
+
+          {/* Shared footer appears below every authenticated page */}
+          <AppFooter />
         </main>
       </div>
     </div>
