0426356c309660599212f28b5ec78ce55e349c54
Natnial Asegdew <natnial99@gmail.com>
2026-09-15 14:09:31 +0300

    import functions

---
+++ backend/src/services/questionService.js
@@ -0,0 +1,6 @@
+import Question from "../models/Question.js";
+import QuestionVector from "../models/QuestionVector.js";
+import { embedContent } from "../ai/gemini.js";
+
+import env from "../config/env.js";
+
