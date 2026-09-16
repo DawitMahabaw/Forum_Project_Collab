2355658a27a531b53b130650d450790a6fea2622
Surafel Jibruk <surafelgk61@gmail.com>
2026-09-16 14:13:53 +0300

    import services

--- backend/src/services/answerService.js
+++ backend/src/services/answerService.js
@@ -6,3 +6,17 @@ const createAnswerService = async ({ questionId, userId, content }) => {
 
 
   const question = await Question.findById(questionId);
+
+  if (!question) {
+    
+    // Create an Error object containing the message.
+    const error = new Error("Question not found.");
+
+    
+    // 404 means "Not Found".
+    error.statusCode = 404;
+
+    // "throw" stops execution and sends the error back
+    // through the application's error-handling flow.
+    throw error;
+  }
