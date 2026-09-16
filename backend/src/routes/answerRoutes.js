26fa874a0dd132794d45bb64d49ce6bf4325e60f
Surafel Jibruk <surafelgk61@gmail.com>
2026-09-16 13:53:39 +0300

    import services

--- backend/src/routes/answerRoutes.js
+++ backend/src/routes/answerRoutes.js
@@ -8,3 +8,14 @@ import {
 } from "../controllers/answerController.js";
 
 import authenticate from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/", authenticate, createAnswer);
+
+
+router.put("/:answerId", authenticate, updateAnswer);
+
+
+
+router.delete("/:answerId", authenticate, deleteAnswer);
