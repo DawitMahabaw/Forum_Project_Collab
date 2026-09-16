145b601ece240a0b9146cbd76d0295cb56861765
Surafel Jibruk <surafelgk61@gmail.com>
2026-09-16 15:24:39 +0300

    import services

--- backend/src/models/Answer.js
+++ backend/src/models/Answer.js
@@ -20,3 +20,33 @@ const Answer = {
 
       return result.insertId;
   },
+
+  async findById(id) {
+    // Execute a SELECT query.
+    const [rows] = await pool.execute(
+      // SELECT chooses which columns we want from the database.
+      `
+      SELECT
+        a.id,
+        a.question_id,
+        a.user_id,
+        a.content,
+        a.created_at,
+        a.updated_at,
+     // These are user columns.
+        //
+        // AS gives them different names in the returned row.
+        //
+        // For example:
+        //
+        // u.user_id
+        // becomes:
+        // author_id
+        //
+        // This prevents confusion between:
+        // answer's user_id
+        // and
+        // author's user_id.
+        u.user_id    AS author_id,
+        u.first_name AS author_first_name,
+        u.last_name  AS author_last_name
