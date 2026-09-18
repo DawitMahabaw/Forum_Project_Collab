3da4889ce3eb00537e8e6c006a9f0ad174defa76
Lexaze <lezaedu1@gmail.com>
2026-09-18 21:25:14 +0300

    add listDocument

--- frontend/src/services/ragService.js
+++ frontend/src/services/ragService.js
@@ -1,2 +1,9 @@
 // Retrieve documents belonging to the authenticated user.
+const listDocuments = async () =>
+  (await api.get("/rag/documents")).data.data || [];
 
+
+  return response.data.data;
+};
+
+export { listDocuments, uploadPdf };
