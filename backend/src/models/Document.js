c126344b6d8075c4fc10a8cb7ffb424d1b9a6b1d
Eliyas Wakjira <eliwak430@gmail.com>
2026-09-19 13:48:15 +0300

    docs(Document): add JSDoc comment for addChunkVector method

--- backend/src/models/Document.js
+++ backend/src/models/Document.js
@@ -130,7 +130,7 @@ return result.affectedRows > 0;
 // Refactor JSDoc documentation formatting for create method
 // Refactor JSDoc documentation formatting for listForUser method
 // Refactor JSDoc documentation formatting for updateStatus method
-
+// Refactor JSDoc documentation formatting for addChunkVector method
 
 
 
